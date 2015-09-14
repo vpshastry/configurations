2c8db25b56067368c60156255cddc71f9f6c746f
Varun Shastry <vshastry@redhat.com>
2015-09-14 13:11:56 -0400

    Some updates
    Signed-off-by: Varun Shastry <vshastry@redhat.com>

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-backup($@) {
+backup() {
   if [ "$1" != "" ]; then
           CONFIG_BACKUP="${1// /-}"
   else
           cur_time=`date`
-          CONFIG_BACKUP="~/config_backup_${cur_time// /-}"
+          CONFIG_BACKUP="/home/`whoami`/config_backup_${cur_time// /-}"
   fi
-  mkdir "$CONFIG_BACKUP"
-  cp ~/.vimrc "$CONFIG_BACKUP/"
-  cp -R ~/.vim/ "$CONFIG_BACKUP/"
-  cp ~/.screenrc "$CONFIG_BACKUP/"
-  cp ~/.gitconfig "$CONFIG_BACKUP/"
-  cp ~/.gdbinit "$CONFIG_BACKUP/"
+
+  mkdir $CONFIG_BACKUP || return -1
+  cp ~/.vimrc "$CONFIG_BACKUP/" || return -1
+  cp -R ~/.vim/ "$CONFIG_BACKUP/" || return -1
+  cp ~/.screenrc "$CONFIG_BACKUP/" || return -1
+  cp ~/.gitconfig "$CONFIG_BACKUP/" || return -1
+  cp ~/.gdbinit "$CONFIG_BACKUP/" || return -1
 }
 
-backup || exit -1
+backup $@ || exit -1
 cp vimrc ~/.vimrc
 cp -R vim ~/.vim/
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 cp screenrc ~/.screenrc
 cp gitconfig ~/.gitconfig
 cp gdbinit ~/.gdbinit
